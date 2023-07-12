e20b0394eb9a228b68dd37bfeee1d2cf201b1ceb
Ademola <sherggs@gmail.com>
2023-07-13 00:36:55 +0100

    HomePage and Exercise detail routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import {Box } from '@mui/material'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import ExerciseDetail from './pages/ExerciseDetail'
+import Homepage from './pages/Homepage'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <Box width="400px" sx={{width: {x1: '1488px'}}} m="auto">
+    <Navbar />
+
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+
+    </Box>
   )
 }
 
