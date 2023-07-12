f871e3ce19c6b29a31b4ab5cd1ac28f799bffad4
Ademola <sherggs@gmail.com>
2023-07-13 00:34:01 +0100

    Hero Banner

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,18 @@
+import { Box, Stack, Typography } from '@mui/material'
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    <Box sx={{
+        mt: {lg: '212px', xs: '70px'},
+        ml: {sm: '50px'},
+    }} position="relative" p="20px">
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+    Fitness Club
+    </Typography>
+        
+    </Box>
+  )
+}
+
+export default HeroBanner
