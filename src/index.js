a6ff9b466c30d95f46902888ab7484b1a1c12fe5
Ademola <sherggs@gmail.com>
2023-07-15 19:56:49 +0100

    Index File

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";  // import React from 'react'
 import ReactDOM from "react-dom"; // import ReactDOM from 'react-dom'
 import App from "./App"; // import App from './App'
+import { BrowserRouter } from "react-router-dom"; // import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));  // const root = document.getElementById('root')
 
-root.render(<App />); 
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root"));  // const root = document.getElementById('root')
+
+root.render(
+<BrowserRouter> 
+<App /> 
+</BrowserRouter>
+); 
+
