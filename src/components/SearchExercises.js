a11b45cfee08f3570853502f7cf5420bef9cb341
Ademola <sherggs@gmail.com>
2023-07-15 19:56:16 +0100

    Search Bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { Box, Button, Stack, TextField, Typography } from '@mui/material'
+
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems='center' mt='37px' justifyContent='center' p='20px'>
+      <Typography fontWeight={700} fontSize='26px' mb='20px'>
+        Awesome Exercises You <br /> Should Know
+      </Typography>
+    </Stack>
   )
 }
 
